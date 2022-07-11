3eb46b1b7ac593bbf996921d06d55a55843ed403
FridayAI <gabi.get@hotmail.com>
2022-07-11 15:10:33 -0300

    Adicionar repositório de veículos

---
+++ DEV-Car-Console/Repository/VehiclesRepository.cs
@@ -0,0 +1,13 @@
+﻿using DEV_Car_Console.Models;
+
+namespace DEV_Car_Console.Repository
+{
+    public class VehiclesRepository
+    {
+        public IList<Vehicle> Vehicles { get; set; }
+        public VehiclesRepository()
+        {
+            Vehicles = new List<Vehicle>();
+        }
+    }
+}
