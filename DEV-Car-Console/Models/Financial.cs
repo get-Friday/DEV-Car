1fcd0313c589638c015a670b0749583b0d4cc27d
FridayAI <gabi.get@hotmail.com>
2022-07-11 19:46:01 -0300

    Adiciona classe financial para repositório de transferências

---
+++ DEV-Car-Console/Models/Financial.cs
@@ -0,0 +1,17 @@
+﻿namespace DEV_Car_Console.Models
+{
+    public class Financial
+    {
+        public Vehicle vehicleData { get; set; }
+        public string BuyerCPF { get; set; }
+        public Decimal Value { get; set; }
+        public DateTime Date { get; set; }
+        public Financial(Vehicle vehicleData, string buyerCPF, decimal value, DateTime date)
+        {
+            this.vehicleData = vehicleData;
+            BuyerCPF = buyerCPF;
+            Value = value;
+            Date = date;
+        }
+    }
+}
---
+++ DEV-Car-Console/Repository/TransferHistoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using DEV_Car_Console.Models;
+
+namespace DEV_Car_Console.Repository
+{
+    public class TransferHistoryRepository
+    {
+        public IList<Financial> TransferHistory { get; set; }
+        public TransferHistoryRepository(IList<Financial> transferHistory)
+        {
+            TransferHistory = transferHistory;
+        }
+    }
+}
