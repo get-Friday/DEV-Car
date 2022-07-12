5d75e87867b3afd8ae8bf737a1117973ab3c94fa
FridayAI <gabi.get@hotmail.com>
2022-07-12 20:36:16 -0300

    Implementa tela veículos disponíveis

--- DEV-Car-Console/Screens/AvailableVehiclesScreen.cs
+++ DEV-Car-Console/Screens/AvailableVehiclesScreen.cs
@@ -7,11 +7,22 @@ namespace DEV_Car_Console.Screens
     {
         public void Start()
         {
-           
-        }
-        public void PrintContent()
-        {
+            IEnumerable<Vehicle> query = VehiclesRepository.Vehicles.Where(vehicle => vehicle.BuyerCPF == null);
             
+            string subHeaderText = "Veículos disponíveis pra compra";
+            int canvasSize = query.Count() * 2 + 10;
+            int row = 7;
+
+            MenuScreen.PrintMenu(canvasSize, 85, subHeaderText);
+
+            foreach (Vehicle vehicle in query)
+            {
+                Console.SetCursorPosition(2, row);
+                Console.WriteLine(vehicle.ShowInfo());
+                row += 2;
+            }
+
+            Console.ReadLine();
         }
     }
 }
